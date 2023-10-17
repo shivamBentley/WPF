a09af36d316376d664105d2833284eb466c4aa66
Shivam Chaurasiya <Shivam.Chaurasiya@bentley.com>
2023-10-17 15:38:58 +0530

    MVVM implementation

---
+++ WPF_MVVM_Implementation/Command/RelayCommand.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WPF_MVVM_Implementation.ViewModel
+{
+    class RelayCommand : ICommand
+    {
+        Action<object> executeAction;
+        Func<object, bool> canExecute;
+        bool canExecuteCache;
+
+
+        public RelayCommand(Action<object> executeAction, Func<object, bool> canExecute, bool canExecuteCache)
+        {
+            this.canExecute = canExecute;
+            this.executeAction = executeAction;
+            canExecuteCache = canExecuteCache;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (canExecute == null)
+            {
+                return true;
+
+            }
+            else
+            {
+                return canExecute(parameter);
+            }
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+
+                CommandManager.RequerySuggested += value;
+
+            }
+            remove
+            {
+
+                CommandManager.RequerySuggested -= value;
+
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            executeAction(parameter);
+        }
+    }
+}
