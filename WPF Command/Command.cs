a9d71f46c310841cb5a82d8f639c8c86688f4441
Shivam Chaurasiya <Shivam.Chaurasiya@bentley.com>
2023-10-17 13:18:19 +0530

    Commands in WPF

---
+++ WPF Command/Command.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPF_Command
+{
+    class Command : ICommand
+    {
+        Action<object> executeMethod;
+        Func<object ,bool > canExecuteMethod;
+
+
+        public Command(Action<object> executeMethod, Func<object ,bool> canExecuteMethod)
+        {
+            this.executeMethod = executeMethod; 
+            this.canExecuteMethod = canExecuteMethod;
+        }
+
+
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            executeMethod(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+    }
+}
---
+++ WPF Command/ViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WPF_Command
+{
+    public class ViewModel
+    {
+        public ICommand MyCommand { get; set; } 
+
+        public ViewModel() {
+
+            MyCommand = new Command(ExecuteMethod, CanExecuteMethod);
+        }
+
+        private bool CanExecuteMethod(object parameter)
+        {
+            return true;
+        }
+
+        private void ExecuteMethod(object parameter)
+        {
+            MessageBox.Show("Command Executed No code behind");
+        }
+    }
+}
